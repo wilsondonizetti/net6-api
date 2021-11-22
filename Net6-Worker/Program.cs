5d48e55fa770685ede38382dc5551e1f2ca6e8d8
Wilson Donizetti <wilsondonizetti@gmail.com>
2021-11-22 11:20:55 -0300

    add worker

--- Net6-Worker/Program.cs
+++ Net6-Worker/Program.cs
@@ -1,9 +1,14 @@
 using Net6_Worker;
 
 IHost host = Host.CreateDefaultBuilder(args)
-    .ConfigureServices(services =>
+    .ConfigureServices((hostContext, services) =>
     {
+        IConfiguration configuration = hostContext.Configuration;
+
         services.AddHostedService<Worker>();
+        services.AddRabbitMQ(configuration);
+        services.AddSingleton<IConsumerMessage<Post>, PostConsumerMessage>();
+                
     })
     .Build();
 
---
+++ Net6-Worker/TestConsumerMessage.cs
@@ -0,0 +1,45 @@
+using Microsoft.Extensions.ObjectPool;
+using Microsoft.Extensions.Options;
+using Net6_RabbitMQ.Consumer;
+using RabbitMQ.Client;
+
+namespace Net6_Common
+{
+    //Essa classe deve ser substituida pelo cenario real
+    public class TestConsumerMessage : ConsumerMessageBase<TestMessage>
+    {        
+        public TestConsumerMessage(
+            IServiceProvider serviceProvider,
+            ObjectPool<IModel> objectPool,
+            IOptions<ConsumerOptions> options,
+            IConfiguration configuration,
+            ILogger<ConsumerMessageBase<TestMessage>> logger)
+        : base(serviceProvider, objectPool, options, configuration, logger)
+        {            
+        }
+
+        protected override async Task<MessageProcessResult> ProcessMessageAsync(
+            TestMessage message, MessageReceivedArgs args, CancellationToken cancellationToken)
+        {
+            try
+            {                
+
+                cancellationToken.ThrowIfCancellationRequested();
+
+                await Task.Run(() =>
+                {
+                    logger.LogInformation("Message received: '{mensagem}'", message);
+
+                    logger.LogInformation("Message read: '{mensagem}'", message);
+                });
+                
+                return MessageProcessResult.Success;
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "Error to process message: {mensagem}", message);
+                return MessageProcessResult.Fail;
+            }
+        }
+    }
+}
